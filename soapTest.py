a1c6169941304609b798cb66d642d4cf9bdd3ff6
Cristian <moino.cristian@gmail.com>
2020-08-06 23:45:49 -0600

    agregar y listar

--- soapTest.py
+++ soapTest.py
@@ -1,19 +1,14 @@
 import json
 from zeep  import Client
 client = Client(wsdl="https://api.softwareavanzado.world/index.php?webserviceClient=administrator&webserviceVersion=1.0.0&option=contact&api=soap&wsdl")
-c_type = client.get_type("id:271")
-cont = 0
+c = 7
+#for i in range(c):
+#        if c <= 10:
+#                print("esto trae",client.service.create("201114527_"+str(c)))
+#        c +=1
 
-for c in client.service.readList():
-    
-    if cont < 10:
+for c in client.service.readList(0,10,201114527):    
+        print(c.name)
 
-        print(c)
-        
-        cont +=1
-
-data_set = {"name": "201114527"}
-json_dump = json.dumps(data_set)
-print(json_dump)
 
 
