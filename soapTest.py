304d27919ec2012ee030f7d924ccc47da95e116c
Cristian <moino.cristian@gmail.com>
2020-08-07 21:22:26 -0600

    arreglos

--- soapTest.py
+++ soapTest.py
@@ -1,15 +1,21 @@
-from zeep  import Client #importamos Client del zeep
-client = Client(wsdl="https://api.softwareavanzado.world/index.php?webserviceClient=administrator&webserviceVersion=1.0.0&option=contact&api=soap&wsdl") #url de acceso al web service
-c = 0 #contador de clientes a ingresar
-for i in range(c): #bucle para crear cliente 
-        if c < 10: #van hacer 10 clientes
-                print("esto trae",client.service.create("201114527_"+str(c))) #llamamos al servicio y colocamos los parametros
-        c +=1 #incremantamos el contador en 1
-
-for c in client.service.readList(0,10,201114527):    #bucle para obtener cliente
-        print(c.name) # imprimimos el nombre del cliente
-
-
+#importamos Client del zeep
+from zeep  import Client 
+#url de acceso al web service
+client = Client(wsdl="https://api.softwareavanzado.world/index.php?webserviceClient=administrator&webserviceVersion=1.0.0&option=contact&api=soap&wsdl") 
+#contador de clientes a ingresar
+c = 0 
+#bucle para crear cliente 
+for i in range(c): 
+        #van hacer 10 clientes
+        if c < 10: 
+                #llamamos al servicio y colocamos los parametros
+                print("esto trae",client.service.create("201114527_"+str(c))) 
+        #incremantamos el contador en 1
+        c +=1 
+#bucle para obtener cliente
+for c in client.service.readList(0,10,201114527):    
+        # imprimimos el nombre del cliente
+        print(c.name) 
 
     
 
