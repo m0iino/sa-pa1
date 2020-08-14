2805cc6acf253b61b93bc6585b640a07b2d92e47
Cristian <moino.cristian@gmail.com>
2020-08-14 16:47:29 -0600

    ingreso de contactos parte 2

--- soapTest.py
+++ soapTest.py
@@ -17,8 +17,17 @@ session.auth = HTTPBasicAuth(user, password)
 # request autenticado
 client = Client(wsdl='https://api.softwareavanzado.world/index.php?webserviceClient=administrator&webserviceVersion=1.0.0&option=contact&api=soap&wsdl',
             transport=Transport(session=session))
-print(client)
+#contador de clientes a ingresar
+#bucle para crear cliente 
+for i in range(0,10): 
+        #van hacer 10 clientes
+        #llamamos al servicio y colocamos los parametros
+                print("esto trae",client.service.create("201114527_"+str(i))) 
 
-
+        
+#bucle para obtener cliente
+for c in client.service.readList(0,10,201114527):    
+        # imprimimos el nombre del cliente
+        print(c.name) 
 
 
