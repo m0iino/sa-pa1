8786d3108a8ef5faf53e4866a03cbbaaadc578bf
Cristian <moino.cristian@gmail.com>
2020-08-14 16:25:57 -0600

    Parte 2

--- soapTest.py
+++ soapTest.py
@@ -1,12 +1,23 @@
-#importamos Client del zeep
-from zeep  import Client 
-#url de acceso al web service
-client = Client(wsdl="https://api.softwareavanzado.world/index.php?option=token&api=oauth2&grant_type=client_credentials&client_id=sa&client_secret=fb5089840031449f1a4bf2c91c2bd2261d5b2f122bd8754ffe23be17b107b8eb103b441de3771745") 
+#importamos autenticación httbasic
+from requests.auth import HTTPBasicAuth
+#importamos session
+from requests import Session
+# importamo zeep
+from zeep import Client
+# porque vamos a utilizar autenticacion zeep necesita de Transport
+from zeep.transports import Transport
+# se crea una sesion para usar en el request
+session = Session()
+#variable de usuario
+user = 'sa'
+#variable contraseña
+password = 'usac'
+# utilizamos la autenticación de HTTPBasicAuth
+session.auth = HTTPBasicAuth(user, password)
+# request autenticado
+client = Client(wsdl='https://api.softwareavanzado.world/index.php?webserviceClient=administrator&webserviceVersion=1.0.0&option=contact&api=soap&wsdl',
+            transport=Transport(session=session))
 print(client)
-
-    
-
-
 
 
 
