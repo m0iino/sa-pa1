13a6a9e6f62d00087688a64bfe3d9986a9c84dae
Cristian <moino.cristian@gmail.com>
2020-08-14 22:51:24 -0600

    practica 2 parte 1

--- soapTest.py
+++ soapTest.py
@@ -1,33 +1,42 @@
-#importamos autenticación httbasic
-from requests.auth import HTTPBasicAuth
-#importamos session
-from requests import Session
-# importamo zeep
-from zeep import Client
-# porque vamos a utilizar autenticacion zeep necesita de Transport
-from zeep.transports import Transport
-# se crea una sesion para usar en el request
-session = Session()
-#variable de usuario
-user = 'sa'
-#variable contraseña
-password = 'usac'
-# utilizamos la autenticación de HTTPBasicAuth
-session.auth = HTTPBasicAuth(user, password)
-# request autenticado
-client = Client(wsdl='https://api.softwareavanzado.world/index.php?webserviceClient=administrator&webserviceVersion=1.0.0&option=contact&api=soap&wsdl',
-            transport=Transport(session=session))
-#contador de clientes a ingresar
-#bucle para crear cliente 
-for i in range(0,10): 
-        #van hacer 10 clientes
-        #llamamos al servicio y colocamos los parametros
-                print("esto trae",client.service.create("201114527_"+str(i))) 
-
-        
-#bucle para obtener cliente
-for c in client.service.readList(0,10,201114527):    
-        # imprimimos el nombre del cliente
-        print(c.name) 
-
-
+#importamos request
+import requests
+import json
+# url para obtener el acces_token
+endpoint = "https://api.softwareavanzado.world/index.php?option=token&api=oauth2"
+#colocamos en el header los valores del body con la llave client_secret
+headers = {"client_id":"sa",
+            "client_secret":"fb5089840031449f1a4bf2c91c2bd2261d5b2f122bd8754ffe23be17b107b8eb103b441de3771745",
+            "grant_type": "client_credentials",
+            }
+#guardamos en un response el request post
+response = requests.post(endpoint, data=headers)
+#imprimirmos el status del response
+print("status cod:", response.status_code)
+print(response.json()["access_token"])
+# guardamos el access_token en el bearer
+bearer = response.json()["access_token"]
+#url para hacer un crear
+endpoint = "https://api.softwareavanzado.world/index.php?webserviceClient=administrator&webserviceVersion=1.0.0&option=contact&api=hal"
+# iteramos 10 porque vamos a crear 10 contactos
+for i in (0,10):
+    #nombre del contacto
+    data = {"name":"201114527_"+str(i)}
+    #agregamos en el header el bearer 
+    headers = { "Authorization": "Bearer "+str(bearer)
+            }
+    #hacemos un request tipo post y lo guardamos en un response
+    response = requests.post(endpoint, data=data, headers=headers )
+    #imprimimos el status del response
+    print("status code:", response.status_code)
+    print(response)
+# url para listar a los contactos
+endpoint = "https://api.softwareavanzado.world/administrator/index.php?webserviceClient=administrator&webserviceVersion=1.0.0&option=contact&api=hal&filter[search]=201114527B"
+#agregamos el bearer al header
+headers = { "Authorization": "Bearer "+str(bearer)
+            }
+# hacemos un request get y lo guardamos en un response
+response = requests.get(endpoint, headers=headers )
+# imprimimos el status del reponse
+print("status code:", response.status_code)
+# imprimimos el listado
+print(response.json())
